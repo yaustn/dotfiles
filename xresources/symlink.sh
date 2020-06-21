2f3920e03e4226911b91b93efff190c3c2e4ae29
Yoo, Austin <yaustn@pm.me>
2020-06-21 00:05:01 -0400

    Update xresources with perl extensions

--- xresources/symlink.sh
+++ xresources/symlink.sh
@@ -3,7 +3,9 @@
 [ -h $HOME/.Xresources ] && rm $HOME/.Xresources
 [ -h $HOME/.xsettingsd ] && rm $HOME/.xsettingsd
 [ -h $HOME/.xinitrc ] && rm $HOME/.xinitrc
+[ -h $HOME/.urxvt/ext ] && rm -rf $HOME/.urxvt/ext
 
 ln -sv $DOTFILES_DIR/xresources/config $HOME/.Xresources
 ln -sv $DOTFILES_DIR/xresources/xsettingsd $HOME/.xsettingsd
 ln -sv $DOTFILES_DIR/xresources/xinitrc $HOME/.xinitrc
+ln -sv $DOTFILES_DIR/xresources/ext $HOME/.urxvt/ext
