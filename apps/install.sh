66e3d55ff10bfbfd1bd93aae56f6ecc165a70d0d
Yoo, Austin <yaustn@pm.me>
2020-12-05 21:56:20 -0500

    Update 20.10

--- apps/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-unzip exa-linux-x86_64-0.9.0.zip
-sudo mv exa-linux-x86_64 /usr/local/bin/exa
-
-#sudo tar -C /usr/local -xzf go1.15.2.linux-amd64.tar.gz
-
-
--- install.sh
+++ install.sh
@@ -2,27 +2,27 @@
 
 echo "Setting environment variables... and directories..."
 export PROJECTS_DIR="$HOME/projects"
-export CODE_DIR="$HOME/code"
+#export CODE_DIR="$HOME/code"
 export DOTFILES_DIR="$PROJECTS_DIR/dotfiles"
 echo "Done.\n"
 
 echo "Creating directory structure"
-rm -rf $HOME/Downloads
-rm -rf $HOME/Desktop
 rm -rf $HOME/Documents
 rm -rf $HOME/Music
 rm -rf $HOME/Pictures
 rm -rf $HOME/Public
 rm -rf $HOME/Templates
 rm -rf $HOME/Videos
-xdg-user-dirs-update
 
+mkdir -p $HOME/Downloads
+mkdir -p $HOME/Desktop
 mkdir -p $HOME/.config
 mkdir -p $HOME/tmp
-mkdir -p $HOME/Downloads
 mkdir -p $HOME/projects
 mkdir -p $HOME/code
 mkdir -p $HOME/go
+
+xdg-user-dirs-update
 echo "Done.\n"
 
 # Install apps and tools
