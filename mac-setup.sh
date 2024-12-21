233e3da9ef370c9060224d8d5a23ce7f79825db0
Austin Yoo <yaustn@pm.me>
2024-12-21 17:30:09 -0500

    update hotkeys

--- mac-setup.sh
+++ mac-setup.sh
@@ -11,6 +11,7 @@ brew install raycast
 brew install iterm2
 brew install visual-studio-code
 brew install calibre
+brew install halloy
 
 # install cli tools
 brew install git
@@ -22,6 +23,7 @@ brew install eza
 brew install tmux
 brew install nvim
 brew install fzf
+brew install pgcli
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -36,5 +38,118 @@ defaults write com.apple.finder ShowStatusBar -bool true # show status bar
 
 killall Finder;
 
-
 ansible-playbook -t dotfiles ansible/setup.yml --ask-become-pass --ask-vault-pass
+
+# VSCODE
+copy settings.json --> /Users/ayoo/Library/Application\ Support/Code/User/settings.json
+copy keybindings.json --> /Users/ayoo/Library/Application\ Support/Code/User/keybindings.json
+
+be5invis.vscode-icontheme-nomo-dark
+golang.go
+vscodevim.vim
+ms-azuretools.vscode-docker
+
+
+
+
+
+ 
+### README
+
+    # Appearance
+    #     Dark Mode
+    #     Show Scroll Bars -> "Always"
+    #         Ugly, but better for web development
+    # Dock
+    #     Remove most applications from Dock
+    #     Automatic Hide
+    #     Smaller Dock
+    #     "Show recent applications in Dock" -> off
+    #     "Show indicators for open applications" -> on
+    #     Battery -> "Show Percentage"
+    # Display
+    #     Nightshift
+    # Security
+    #     Touch ID
+    # Notifications
+    #     Off, except for Calendar
+    # Siri
+    #     Disabled
+    # Trackpad
+    #     Tap to Click
+    #     Point & Click -> Look up & data detectors off
+    #     More Gestures -> Notification Centre off
+    # Keyboard
+    #     Text
+    #         disable "Capitalise word automatically"
+    #         disable "Add full stop with double-space"
+    #         disable "Use smart quotes and dashes"
+    #         use " for double quotes
+    #         use ' for single quotes
+    #     Keyboard -> Mission Control -> disable all
+    #     Press FN to -> "Do Nothing"
+    #     Keyboard Shortcuts -> Spotlight -> CMD + Space disable
+    #         We will be using Raycast instead
+    # Mission Control
+    #     Hot Corners: disable all
+    # Finder
+    #     General
+    #         New Finder windows show: [Downloads]
+    #         Show these items on the desktop: disable all
+    #     Sidebar:
+    #         activate all Favorites
+    #         move Library to Favorites
+    #     Show only:
+    #         Desktop
+    #         Downloads
+    #         Documents
+    #         [User]
+    #         Library
+    #     Tags
+    #         disable all
+    #     Advanced
+    #         Show all Filename Extensions
+    #         Remove Items from Bin after 30 Days
+    #         View -> Show Preview (e.g. image files)
+    # Sharing
+    #     "Change computer name"
+    #         Also terminal:
+    #             sudo scutil --set ComputerName "newname"
+    #             sudo scutil --set LocalHostName "newname"
+    #             sudo scutil --set HostName "newname"
+    #     "Make sure all file sharing is disabled"
+    # Security and Privacy
+    #     Turn on FileVault
+    #     Add Browser to "Screen Recording"
+    # Storage
+    #     Remove Garage Band & Sound Library
+    #     Remove iMovie
+    # Trackpad
+    #     Speed: Max
+    # Accessibility
+    #     Scroll Speed: Max
+
+
+
+
+    # Make iterm2 Default Term
+    # Preferences ->
+    #     General -> Window
+    #         unselect "Native full screen windows"
+    #         select "close windows when closing an app"
+    #     Appearance ->
+    #         Windows
+    #             select "Hide scrollbars"
+    #         Tabs
+    #             unselect "Show tab bar in fullscreen"
+    #         Dimming
+    #             Unselect all dimming
+    #     Profiles -> Window
+    #         Transparency: 30
+    #         Style: Full Screen
+    #         Screen: Main Screen
+    #     Profiles -> Advanced
+    #         Semantic History -> Open with editor ... -> VS Code
+    #     Open new split pane with current directory
+    #     Natural Text Editing
+    # Bring it to fullscreen Command + Enters
