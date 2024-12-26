66ba3a061bd15d9424e86be1608dba6bd379bf1c
Austin Yoo <yaustn@pm.me>
2024-12-26 11:08:02 -0500

    sync

--- vscode/symlink.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ln -sfv $DOTFILES_DIR/vscode/settings.json $HOME/.config/Code/User/settings.json
-ln -sfv $DOTFILES_DIR/vscode/keybindings.json $HOME/.config/Code/User/keybindings.json
-
---
+++ vscode/sync.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cp ~/Library/Application\ Support/Code/User/settings.json settings.json
+cp ~/Library/Application\ Support/Code/User/keybindings.json keybindings.json
